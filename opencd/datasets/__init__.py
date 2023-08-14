457be33beee4274d3090c89a02d8afd31050ff59
Agustin <48933518+AgustinNormand@users.noreply.github.com>
2023-08-14 10:38:42 -0300

    Agrego los archivos de 3 bandas

--- opencd/datasets/__init__.py
+++ opencd/datasets/__init__.py
@@ -9,7 +9,8 @@ from .rsipac_cd import RSIPAC_CD_Dataset
 from .s2looking import S2Looking_Dataset
 from .second import SECOND_Dataset
 from .svcd import SVCD_Dataset
+from .chaco import CHACO_CD_Dataset
 
 __all__ = ['BaseCDDataset', 'BaseSCDDataset', 'LEVIR_CD_Dataset', 'S2Looking_Dataset', 
            'SVCD_Dataset', 'RSIPAC_CD_Dataset', 'CLCD_Dataset', 'DSIFN_Dataset', 
-           'SECOND_Dataset', 'Landsat_Dataset', 'BANDON_Dataset']
+           'SECOND_Dataset', 'Landsat_Dataset', 'BANDON_Dataset', 'CHACO_CD_Dataset']
